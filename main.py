3ca0a1e27492522a5a8e3bc73c0cf05b8cdb4c9b
Pralov Malla <pralov.malla123@gmail.com>
2026-03-30 10:43:12 +0545

    Generated audio succesfully with kokoro-tts

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import soundfile as sf
+from pathlib import Path
 import torch
+from kokoro import KPipeline
 
-print("GPU:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU available")
+OUTPUT_DIR = Path("test_generated_sounds")
+OUTPUT_DIR.mkdir(exist_ok=True)
+
+print(torch.cuda.is_available(), torch.cuda.get_device_name(0))
+
+pipeline = KPipeline(lang_code="a", device="cuda")
+
+for i, (gs, ps, audio) in enumerate(
+    pipeline("Hi, I am pratik lama and I am your visa interview officer.", voice="af_heart")
+):
+    sf.write(OUTPUT_DIR / f"out_{i}.wav", audio, 24000)
